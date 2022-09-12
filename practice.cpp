4247b46e2ab05d19936edb829ef9c1796fc51943
Bhuvneesh-Sayal <bhuvneeshsayal@gmail.com>
2022-09-13 00:13:21 +0530

    first commit

---
+++ practice.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Multiplication of 2D array or matrix
+    /*
+    int A[3][5] = {{1,2,3,1,2}, {2,2,3,4,2}, {3,2,2,1,2}}, B[5][2] = {{2,2}, {3,4}, {1,2}, {2,5}, {1,4}}, C[3][2];
+    int n = 3, m = 5, p = 2;
+    for(int i = 0; i<n; i++){
+        for(int k = 0; k<p; k++){
+            C[i][k] = 0;
+            for(int j = 0; j<m; j++){
+                C[i][k] += A[i][j]*B[j][k];
+            }
+            cout<<"C[" << i <<"][" << k << "]: " << C[i][k]<<std::endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+    */
+
+   // Sorting Algorithms
+   
+
+}
